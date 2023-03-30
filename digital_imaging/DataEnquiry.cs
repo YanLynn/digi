54770581a27313316482fbe3db03f152d7bd0564
unknown <yan.lynn1991@gmail.com>
2023-03-30 14:11:00 +0800

    update

---
+++ digital_imaging/DataEnquiry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace digital_imaging
+{
+    public partial class DataEnquiry : Form
+    {
+        public DataEnquiry()
+        {
+            InitializeComponent();
+        }
+    }
+}
